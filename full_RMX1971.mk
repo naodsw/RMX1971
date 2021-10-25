2c0cff0451a0cbbbed18171c6e4dc4c53bda5579
Naufal Altaf <naodesuyo@outlook.com>
2021-10-25 13:46:24 +0000

    RMX1971: Inherit AOSP telephony configurations
    Change-Id: I74d02b2e3ca98bb5f8b3f8267861b5080de31baf

--- full_RMX1971.mk
+++ full_RMX1971.mk
@@ -19,7 +19,7 @@
 #
 
 # Inherit common AOSP configurations
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit product configurations
 $(call inherit-product, device/realme/RMX1971/device.mk)
