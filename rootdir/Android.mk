5b15624d205b5230f32d9b3624cac7583f325537
Naufal Altaf <naodesuyo@outlook.com>
2021-10-26 06:46:26 +0000

    RMX1971/rootdir: Fix module path
    Change-Id: I7f5b6b61ed451cabcdf69c3fb570eff19bd660c0

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -5,7 +5,7 @@ LOCAL_MODULE       := fstab
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/fstab.qcom
-LOCAL_MODULE_PATH  := $(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -13,7 +13,7 @@ LOCAL_MODULE       := init.logcat
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.logcat.rc
-LOCAL_MODULE_PATH  := $(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/init/hw
+LOCAL_MODULE_PATH  := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -21,5 +21,5 @@ LOCAL_MODULE       := init.qcom
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.rc
-LOCAL_MODULE_PATH  := $(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/init/hw
+LOCAL_MODULE_PATH  := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/init/hw
 include $(BUILD_PREBUILT)
