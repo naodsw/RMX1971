451a23c60dda62ffab139d69bc98120c8a73fb3d
Naufal Altaf <naodesuyo@outlook.com>
2021-10-26 01:09:01 +0000

    RMX1971: Build required packages
    Change-Id: I63a8aa221fb31459d0a737c001ccb5427d0456ef

--- device.mk
+++ device.mk
@@ -24,16 +24,44 @@ DEVICE_PATH := device/realme/RMX1971
 # Use the non-open-source parts, if they're present
 $(call inherit-product-if-exists, vendor/realme/RMX1971/device-vendor.mk)
 
+# AAPT config
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
+# Audio
+PRODUCT_PACKAGES += \
+    audio.a2dp.default \
+    libaacwrapper
+
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.manager@1.0
+
+# IMS & Telephony
+PRODUCT_PACKAGES += \
+    ims-ext-common \
+    ims_ext_common.xml \
+    qti-telephony-hidl-wrapper \
+    qti_telephony_hidl_wrapper.xml \
+    qti-telephony-utils \
+    qti_telephony_utils.xml \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.ims.xml
 
 # Input
 PRODUCT_COPY_FILES += $(DEVICE_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/gpio-keys.kl
 
 # Kernel
 $(call inherit-product, $(DEVICE_PATH)-kernel/kernel.mk)
+
+# Perf
+PRODUCT_PACKAGES += vendor.qti.hardware.perf@2.0
 
 # Rootdir
 PRODUCT_PACKAGES += fstab
@@ -44,3 +72,20 @@ PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
 # VNDK
 PRODUCT_SHIPPING_API_LEVEL := 28
 PRODUCT_TARGET_VNDK_VERSION := 29
+
+# WiFi
+PRODUCT_PACKAGES += \
+    TetheringConfigOverlay \
+    WifiOverlay
+
+# WiFi Display
+PRODUCT_PACKAGES += \
+    libavservices_minijail \
+    libdisplayconfig.qti \
+    libminijail \
+    libnl \
+    libqdMetaData \
+    libwfdaac_vendor \
+    vendor.display.config@1.9
+
+PRODUCT_BOOT_JARS += WfdCommon
